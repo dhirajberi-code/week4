e44fef6f67b31813c2a8c83fea80b1be19dbcb99
smitparmar33 <79826232+smitparmar33@users.noreply.github.com>
2021-03-05 10:33:51 +0530

    Link added + dashboard fixed

--- Third_party_Organization/urls.py
+++ Third_party_Organization/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('ThirdPartyOrganizationDashboard/',views.ThirdPartyOrganizationDashboard),
-    path('ThirdPartyOrganizationHome/',views.ThirdPartyOrganizationHome),
+    path('ThirdPartyOrganizationDashboard/',views.ThirdPartyOrganizationDashboard,name='ThirdPartyOrganizationDashboard'),
+    path('ThirdPartyOrganizationHome/',views.ThirdPartyOrganizationHome,name='ThirdPartyOrganizationHome'),
 ]
